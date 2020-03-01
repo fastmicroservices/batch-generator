ffca377d598f22617a7b043552f4f42a5d019f76
Ignacio Rodriguez <inz@fastmicroservices.com>
2020-03-01 10:31:09 +0700

    removed command option and fast processing

--- index.js
+++ index.js
@@ -4,14 +4,11 @@ const Set = require('./unicity-by-set')
 const seed = require('./seed')
 const hash = require('./code-hex-by-hash')
 const ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_='
-const reencoder = require('./code-reencode')(ALPHABET, 9)
+const reencoder = require('./code-reencode')(ALPHABET, 10)
 
-const count = process.argv[2] || 10000000
-const perPass = Math.ceil(count / ALPHABET.length)
-for (let c in ALPHABET) {
-    let set = new Set()
-    let g = generator(set, seed, hash, reencoder)
-    for (let x = 0; x < perPass; x++) {
-        console.log(ALPHABET[c] + g.next().value)
-    }
+const count = 10000000
+let set = new Set()
+let g = generator(set, seed, hash, reencoder)
+for (let x = 0; x < count; x++) {
+    console.log(g.next().value)
 }
