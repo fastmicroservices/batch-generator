3342d466b50862da178fb2d2ccda251fe227c008
Ignacio Rodriguez <inz@fastmicroservices.com>
2020-03-02 11:42:05 +0700

    simplified generator interface

--- generator.js
+++ generator.js
@@ -1,4 +1,16 @@
-module.exports = function* (workingSet, seed, hash, reencoder, reportCollision) {
+const Set = require('./unicity-by-set')
+const Seed = require('./seed')
+const Hash = require('./code-hex-by-hash')
+const Reencoder = require('./code-reencode')
+
+module.exports = function* (workingSetOrAlphabet, seedOrLength, reportCollision, hash, reencoder) {
+  let {workingSet, seed} = {workingSetOrAlphabet, seedOrLength}
+  hash = hash || Hash
+  if (typeof workingSetOrAlphabet === 'string') {
+    workingSet = new Set()
+    seed = Seed
+    reencoder = Reencoder(workingSetOrAlphabet, seedOrLength || 10)
+  }
   reportCollision = reportCollision || (() => {})
   for (;;) {
     const value = reencoder(hash(seed()))
--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const generator = require('./generator')
-
-const Set = require('./unicity-by-set')
-const seed = require('./seed')
-const hash = require('./code-hex-by-hash')
 const ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_='
-const reencoder = require('./code-reencode')(ALPHABET, 10)
 const Reporter = require('./reporter')
 
 const count = 10000000
-const set = new Set()
 let collisions = 0
-const g = generator(set, seed, hash, reencoder, () => {
+const reportCollision = () => {
     process.stderr.write('*')
     collisions++
-})
+}
+const g = generator(ALPHABET, 10, reportCollision)
 const reportingStep = Math.ceil(count / 200)
 const reportProgress = Reporter(count, process.stderr)
 for (let x = 0; x < count; x++) {
