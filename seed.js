b6f5127fc32d17c39c482df39b753fe756f7b353
Ignacio Rodriguez <inz@fastmicroservices.com>
2020-03-02 12:00:20 +0700

    added salt

--- seed.js
+++ seed.js
@@ -1,5 +1,6 @@
 let _global_count = 0
+const SALT = ' bottles on ' + process.env['HOSTNAME']
 
 module.exports = function () {
-  return (++_global_count).toString() + Math.random()
+  return (++_global_count).toString() + Math.random() + SALT
 }
